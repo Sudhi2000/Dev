ebf4ac63f95333e61bbba9033ae46a786af80380
Sudheesh-2000 <143933138+Sudhi2000@users.noreply.github.com>
2025-06-20 15:28:31 +0530

    Sattva_loca_test2
    Sattva_loca_test2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -13,7 +13,8 @@ export const environment = {
   OPENAI_API_KEY:'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',
   j_username:"uat_redefine",
   j_password:"ce07df36-fb9c-4b53-abcc-ba58b30fb622",
-  report_location:"redeftec"
+  report_location:"redeftec",
+  publicSurveyToken: '7b2f6f6d1d548e5fa889866cef51935131846362dc304860094f60d74d9a79c847293f0672aaef1d8cae2720b227d09add4af310dcb0589850eb7f2d3d7452dbc53727196eef310294fafc994a792b6ccf099e01efa94219f7016e94aa45951e16c39dba21c8ab0461c6fac3746f5e8874ff6ef762e453f550786187cb2e5c26'
 
 
 
